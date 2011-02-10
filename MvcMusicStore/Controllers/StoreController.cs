c6eb1a8aa3ea7b3ea7877aa65f9560634d9b6633
russelyang <russelyang@gmail.com>
2011-02-10 09:10:21 -0800

    part 3 finished

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre {Name = "Rock"}
+            };
+            return View(genres);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var generModel = new Genre { Name = genre };
+            return View(generModel);
+        }
+
+    }
+}
